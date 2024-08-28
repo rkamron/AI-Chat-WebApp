bd2f41aca0e7b0b613456c905489d66af6630f27
Kamronbek Rakhimov <156855800+rkamron@users.noreply.github.com>
2024-08-28 14:26:31 -0400

    working on image generation

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -3,7 +3,7 @@
 import axios from "axios";
 import * as z from "zod";
 import { Heading } from "@/components/heading";
-import { MessageSquare } from "lucide-react";
+import { Code } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
@@ -18,8 +18,10 @@ import { Empty } from "@/components/empty";
 import { Loader } from "@/components/loader";
 import { UserAvatar } from "@/components/user-avatar";
 import { BotAvatar } from "@/components/bot-avatar";
+import ReactMarkdown from 'react-markdown'
 
-const ConversationPage = () => {
+
+const CodePage = () => {
     const router = useRouter();
     const [messages, setMessages] = useState<OpenAI.Chat.ChatCompletionMessageParam[]
   >([]);
@@ -43,7 +45,7 @@ const ConversationPage = () => {
             };
             const newMessages = [...messages, userMessage];
 
-            const response = await axios.post("/api/conversation", {
+            const response = await axios.post("/api/code", {
                 messages: newMessages,
             });
 
@@ -61,11 +63,11 @@ const ConversationPage = () => {
     return (
         <div>
             <Heading 
-                title="Conversation"
-                description="Advanced Conversation Model."
-                icon={MessageSquare}
-                iconColor="text-violet-500"
-                bgColor="bg-violet-500/10"
+                title="Code Generation"
+                description="Generate code with descriptive text."
+                icon={Code}
+                iconColor="text-green-700"
+                bgColor="bg-green-700/10"
             />
             <div className="px-4 lg:px-8">
                 <div>
@@ -110,9 +112,21 @@ const ConversationPage = () => {
                                 key={String(message.content)} className={cn("p-8 w-flex flex items-start gap-x-8 rounded-lg", message.role === "user" ? "bg-white border border-black border-opacity-10" : "bg-muted")}
                             >
                                 {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
-                                <p className="text-sm">
-                                    {String(message.content)}
-                                </p>
+                                <ReactMarkdown components={{
+                                    pre: ({ node, ...props}) => (
+                                        <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                                            <pre {...props} />
+                                        </div>
+                                    ), 
+                                    code: ({ node, ...props}) => (
+                                        <code className="bg-black/10 rounded-lg p-1" {...props} />
+                                    )
+                                    }}
+                                    className="text-sm overflow-hidden leading-7"
+                                
+                                >
+                                    {String(message.content) || ""}
+                                </ReactMarkdown>
                             </div>
                         ))}
                     </div>
@@ -122,4 +136,4 @@ const ConversationPage = () => {
     );
 }
 
-export default ConversationPage;
+export default CodePage;
